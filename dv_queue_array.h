2256814474c01edbd49d5628acfbbcc64c33853d
Dev Vrat <itsdvvs.18@gmail.com>
2018-04-16 03:25:22 +0530

    Header file for queue implemention (via Linked List and Array)

--- dv_queue_array.h
+++ dv_queue_array.h
@@ -28,7 +28,7 @@ public:
         intTypeQ = new int[Size];           //memory allocation of queue
     }
 
-    void insert(int value) {
+    void insert(int value) override {
         if(rear == -1){
             intTypeQ[++rear] = value;
             front++;
@@ -38,16 +38,129 @@ public:
         else
             intTypeQ[++rear] = value;
     }
-    void remove() {
+    void remove() override {
         if(front == -1 || front == rear + 1)
             std::cout<<"Underflow"<<std::endl;
         else
             front++;
     }
-    void display(){
+    void display() override {
         int i=front;
         while(i <= rear)
             std::cout<<intTypeQ[i++]<<"  ";
         std::cout<<std::endl;
+    }
+};
+//Array implementaion Ends
+
+
+/************** Implementation via Linked List ***********************/
+//Queue unit structure
+struct QElement{
+    int data = 0;
+    short priority = 3;
+    QElement* next = nullptr;
+};
+
+//Interface
+class QLList{
+protected:
+    short counter, Size;
+    QElement* start, * front, *rear;
+
+public:
+    QLList() : counter(0), Size(0), start(nullptr), front(nullptr), rear(nullptr){}
+    //Member Functions
+    virtual void insert(int value, short prior = 3){
+        if(counter == Size)
+            std::cout<<"Overflow"<<std::endl;
+        else{
+            QElement* New_Elem = new QElement;
+            New_Elem->data = value;
+            New_Elem->priority = prior;
+            if( start == nullptr){
+                start = New_Elem;
+                front = New_Elem;
+                rear = New_Elem;
+                counter++;
+            }
+            else{
+                if(New_Elem->priority > rear->priority){
+                    rear->next = New_Elem;
+                    rear = New_Elem;
+                }
+                else if(New_Elem->priority < front->priority){
+                    New_Elem->next = front;
+                    front = New_Elem;
+                    start = front;
+                }
+                else {
+                    QElement *traverse = start;
+                    while (traverse->next != nullptr && traverse->next->priority <= New_Elem->priority)
+                        traverse = traverse->next;
+                    New_Elem->next = traverse->next;
+                    traverse->next = New_Elem;
+                }
+                counter++;
+            }
+        }
+    };
+    virtual void remove(){
+        if(front == nullptr)
+            std::cout<<"Underflow"<<std::endl;
+        else{
+            front = front->next;
+            delete start;
+            start = front;
+        }
+    };
+    virtual void display(){
+        if(start == nullptr)
+            std::cout<<"No element"<<std::endl;
+        else{
+            QElement* traverse = front;
+            while(traverse != nullptr) {
+                std::cout << traverse->data << "\t";
+                traverse = traverse->next;
+            }
+            std::cout<<std::endl;
+        }
+    };
+};
+
+//Linear Queue Class
+class LinearQL : public QLList{
+public:
+    LinearQL(){
+        std::cout<<"Enter the size of Linear queue"<<std::endl;
+        std::cin>>Size;
+    }
+};
+
+//Circular Queue Class
+class CircularQL : public QLList{
+public:
+    CircularQL(){
+        std::cout<<"Enter the size of Circular queue"<<std::endl;
+        std::cin>>Size;
+    }
+    //Member functions overriding
+    void remove() override {
+        if(counter == 0)
+            std::cout<<"Underflow"<<std::endl;
+        else{
+            front = front->next;
+            delete start;
+            start = front;
+            counter--;
+        }
+    }
+};
+
+class PriorityQL : public QLList{
+public:
+    PriorityQL(){
+        std::cout<<"Enter the size of Priority queue"<<std::endl;
+        std::cin>>Size;
     }
 };
